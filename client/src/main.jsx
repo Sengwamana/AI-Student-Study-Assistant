652e5ebe4210a1e472f4d62f909ef8a4727fd31a
Sengwamana Emeran <158058272+Sengwamana@users.noreply.github.com>
2026-02-08 09:10:03 +0200

    Add ClerkProvider for authentication management
    Integrate ClerkProvider for authentication and update routing structure.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
+
 import Homepage from "./routes/homepage/Homepage";
 import DashboardPage from "./routes/dashboardPage/DashboardPage";
 import ChatPage from "./routes/chatPage/ChatPage";
@@ -10,33 +12,20 @@ import DashboardLayout from "./layouts/dashboardLayout/DashboardLayout";
 import SignInPage from "./routes/signInPage/signInPage";
 import SignUpPage from "./routes/signUpPage/signUpPage";
 
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
 const router = createBrowserRouter([
   {
     element: <RootLayout />,
     children: [
-      {
-        path: "/",
-        element: <Homepage />,
-      },
-      {
-        path: "/sign-in/*",
-        element: <SignInPage />,
-      },
-      {
-        path: "/sign-up/*",
-        element: <SignUpPage />,
-      },
+      { path: "/", element: <Homepage /> },
+      { path: "/sign-in/*", element: <SignInPage /> },
+      { path: "/sign-up/*", element: <SignUpPage /> },
       {
         element: <DashboardLayout />,
         children: [
-          {
-            path: "/dashboard",
-            element: <DashboardPage />,
-          },
-          {
-            path: "/dashboard/chats/:id",
-            element: <ChatPage />,
-          },
+          { path: "/dashboard", element: <DashboardPage /> },
+          { path: "/dashboard/chats/:id", element: <ChatPage /> },
         ],
       },
     ],
@@ -45,6 +34,14 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ClerkProvider
+      publishableKey={clerkPubKey}
+      signInUrl="/sign-in"
+      signUpUrl="/sign-up"
+      afterSignInUrl="/dashboard"
+      afterSignUpUrl="/dashboard"
+    >
+      <RouterProvider router={router} />
+    </ClerkProvider>
   </React.StrictMode>
 );
